99853bcdbd661f6b24b39701a64d27f86008c09b
gvnrxd <208095437+gvnxr@users.noreply.github.com>
2026-02-26 18:32:09 -0500

    Added printMan function to draw hangman when guess is wrong
    Increased number of attemps to max 6

--- game.c
+++ game.c
@@ -10,11 +10,31 @@ void myGuess(char g[20]){
     scanf("%19s", g);
 };
 
+void printMan(int tries){
+    if (tries == 1) {
+        printf(" o\n");
+    }
+    else if(tries == 2) {
+        printf(" o\n |\n");
+    } else if (tries == 3) {
+        printf(" o\n/|\n");
+    }
+    else if (tries == 4) {
+        printf(" o\n/|\\ \n");
+    }
+    else if (tries == 5) {
+          printf(" o\n/|\\\n/\n");
+    }
+      else if (tries == 6) {
+          printf(" o\n/|\\\n/ \\\n");
+    }
+}
+
 int main() {
     srand(time(NULL));
     int num = (rand() % 10);
     int delay = (rand() % 2 + 1);
-    int attempts = 0;
+    int attempts = 1;
     char guess[20];
     int i = 0;
     char wordsArr[10][20] = {
@@ -53,18 +73,20 @@ int main() {
     }   
 
     printf("\n");
-    while(attempts < 3) {
+    while(attempts <= 6) {
         myGuess(guess); 
-        printf("\nYou entered %s\n", guess);
         if (strcmp(guess, wordsArr[num]) == 0) {
             printf("Congrats you won!\n");
             return 0;
         }else {
-            printf("\nTry again! Tries left: %d\n", (2 - attempts));
+            if (attempts <= 6) {
+            printf("\nTry again! Tries left: %d\n", (6 - attempts));
+            printMan(attempts);
+            } 
             attempts++;
         };
     }
-
+    printf("\nGame over");
     printf("\nYour word was %s", wordsArr[num]);
     return 0;
 }
